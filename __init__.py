84de2c02e6510e779e62ac56d47d6e505cda61cf
Rich Colburn <the3dadvantage@gmail.com>
2019-08-14 13:38:27 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,41 +2,39 @@
 # File __init__.py
 #----------------------------------------------------------
  
-#    Addon info
+# If you are reading this you are a rare person indeed.
+# Perhaps you need a little more time on the grill
+ 
+# Addon info
 bl_info = {
-    "name": "Modeling Cloth",
+    "name": "Wrapping Tools",
     "author": "Rich Colburn, email: the3dadvantage@gmail.com",
     "version": (1, 0),
-    "blender": (2, 78, 0),
-    "location": "View3D > Extended Tools > Modeling Cloth",
-    "description": "Maintains the surface area of an object so it behaves like cloth",
-    "warning": "There might be an angry rhinoceros behind you",
+    "blender": (2, 80, 0),
+    "location": "View3D > Extended Tools",
+    "description": "Tools for deforming around complex surfaces",
+    "warning": "If God loses self-confidence He will be an atheist",
     "wiki_url": "",
     "category": '3D View'}
+
 if "bpy" in locals():
     import imp
-    imp.reload(ModelingCloth)
-    imp.reload(SurfaceFollow)
-    imp.reload(UVShape)
-    imp.reload(DynamicTensionMap)
-    print("Reloaded multifiles")
+    imp.reload(SurfaceFollow28)
+    imp.reload(UVShape28)
+    print("Reloaded UV Shape and Surface Follow")
 else:
-    from . import ModelingCloth, SurfaceFollow, UVShape, DynamicTensionMap
-    print("Imported multifiles")
+    from . import SurfaceFollow28, UVShape28
+    print("Imported Surface Follow and UV Shape")
 
    
 def register():
-    ModelingCloth.register()
-    SurfaceFollow.register()
-    UVShape.register()
-    DynamicTensionMap.register()
+    SurfaceFollow28.register()
+    UVShape28.register()
 
     
 def unregister():
-    ModelingCloth.unregister()
-    SurfaceFollow.unregister()
-    UVShape.unregister()
-    DynamicTensionMap.unregister()
+    SurfaceFollow28.unregister()
+    UVShape28.unregister()
 
     
 if __name__ == "__main__":
